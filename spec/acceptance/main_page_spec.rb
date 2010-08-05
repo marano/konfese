d18d4d7fa2eaadeb52c40d32857eb2a327de16bb
marano <thiagomarano@gmail.com>
2010-08-05 18:45:34 -0300

    Will now display secrets on the main page. Better style and title on the main page.

--- spec/acceptance/main_page_spec.rb
+++ spec/acceptance/main_page_spec.rb
@@ -2,13 +2,13 @@ require 'spec_helper'
 
 describe 'main page' do
   before { visit root_path }
-  context 'submiting a new post' do
+  context 'submiting a new secret' do
     before do
       fill_in 'post[body]', :with => 'My secret.'
       click 'Post'
     end
-    it 'should save the new post' do
-      Post.count.should be 1
+    it 'should display the new secret' do
+      page.should have_content 'My secret.'
     end
   end
 end
