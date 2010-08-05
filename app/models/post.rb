fd72fc48e1fd04e6c3dc88bf72c399d1587d5ba9
marano <thiagomarano@gmail.com>
2010-08-05 20:28:55 -0300

    Now secrets are ordered by publication date.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,4 @@
 class Post < ActiveRecord::Base
   validates_presence_of :body
+  default_scope :order => 'created_at DESC'
 end
--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -2,4 +2,14 @@ require 'spec_helper'
 
 describe Post do
   it { should validate_presence_of :body }
+  
+  context 'when secrets are posted' do
+    before do
+      @first_secret = Factory :post
+      @second_secret = Factory :post
+    end
+    it 'the one published later should come first' do
+      Post.first.should == @second_secret
+    end
+  end
 end
