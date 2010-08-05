2174dc007cc40efa111efb4dec266d192ed11264
marano <thiagomarano@gmail.com>
2010-08-05 18:41:43 -0300

    Root action will now expose posts.

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,5 +1,6 @@
 class MainController < ApplicationController
   def index
+    @posts = Post.all
   end
 end
 
--- spec/controllers/main_controller_spec.rb
+++ spec/controllers/main_controller_spec.rb
@@ -1,9 +1,16 @@
 require 'spec_helper'
 
 describe MainController do
-
-  it 'should have a main page' do
-    get :index
-    response.should render_template(:index)
+  context 'with a secret told' do
+    before { Factory :post }
+    context 'at the root page' do
+      before { get :index }
+      it 'should render the index template' do
+        response.should render_template :index
+      end
+      it 'should assign secrets' do
+        assigns[:posts].should_not be_empty
+      end
+    end
   end
 end
